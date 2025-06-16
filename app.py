f7bcd245efa25aa1df11132571d011fa03f1fb20
Yopp <desilvajoner99@gmail.com>
2025-06-16 15:00:18 +0800

    fixes

--- app.py
+++ app.py
@@ -280,4 +280,4 @@ def process_coughs():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
+    app.run(debug=True, use_reloader=True) 
